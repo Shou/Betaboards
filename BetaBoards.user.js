64098f93abe41685096e1a667f17e8a013812ae4
Shou <x@shou.io>
2013-11-26 06:45:43 +0000

    version bump again uguu

--- BetaBoards.user.js
+++ BetaBoards.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            BetaBoards
 // @description     It's just like IRC now
-// @version         0.4.1
+// @version         0.4.2
 // @include         http*://*.zetaboards.com/*
 // @author          Shou
 // @copyright       2013, Shou
